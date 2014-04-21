6b127a34bfe17439f2efb65e25539e9eefc2a1ab
christopherfan <chrisfan@outlook.com>
2014-04-20 21:48:24 -0700

    Transformed Cluster Data

--- ClusterDictionaryTest.py
+++ ClusterDictionaryTest.py
@@ -7,7 +7,7 @@ class ClusterDictionaryTest(unittest.TestCase):
 	# Failing Unit Test
 	def setUp(self):
 		self.dict = ClusterDictionary()
-		self.default_entry= [('0_0', 0), ('0_1', 0), ('0_2', 0), ('0_3', 0), ('0_4', 0), ('0_5', 0), ('0_6', 0), ('0_7', 0), ('0_8', 0), ('0_9', 0), ('0_10', 0), ('0_11', 0), ('1_0', 0), ('1_1', 0), ('1_2', 0), ('1_3', 0), ('1_4', 0), ('1_5', 0), ('1_6', 0), ('1_7', 0), ('1_8', 0), ('1_9', 0), ('1_10', 0), ('1_11', 0), ('2_0', 0), ('2_1', 0), ('2_2', 0), ('2_3', 0), ('2_4', 0), ('2_5', 0), ('2_6', 0), ('2_7', 0),('2_8', 0), ('2_9', 0), ('2_10', 0), ('2_11', 0), ('3_0', 0), ('3_1', 0), ('3_2', 0), ('3_3', 0), ('3_4', 0), ('3_5', 0), ('3_6', 0), ('3_7', 0), ('3_8', 0), ('3_9', 0), ('3_10', 0), ('3_11', 0), ('4_0', 0), ('4_1', 0), ('4_2', 0), ('4_3', 0), ('4_4', 0), ('4_5', 0), ('4_6', 0), ('4_7', 0), ('4_8', 0), ('4_9', 0), ('4_10', 0), ('4_11', 0), ('5_0', 0), ('5_1', 0), ('5_2', 0), ('5_3', 0), ('5_4', 0), ('5_5', 0), ('5_6', 0), ('5_7', 0), ('5_8', 0), ('5_9', 0), ('5_10', 0), ('5_11', 0)]	
+		self.default_entry= [('1_1', 0), ('1_2', 0), ('1_3', 0), ('1_4', 0), ('1_5', 0), ('1_6', 0), ('1_7', 0), ('1_8', 0), ('1_9', 0), ('1_10', 0), ('1_11', 0), ('1_12', 0), ('2_1', 0), ('2_2', 0), ('2_3', 0), ('2_4', 0), ('2_5', 0), ('2_6', 0), ('2_7', 0), ('2_8', 0), ('2_9', 0), ('2_10', 0), ('2_11', 0), ('2_12', 0), ('3_1', 0), ('3_2', 0), ('3_3', 0), ('3_4', 0), ('3_5', 0), ('3_6', 0), ('3_7', 0), ('3_8', 0), ('3_9', 0), ('3_10', 0), ('3_11', 0), ('3_12', 0), ('4_1', 0), ('4_2', 0), ('4_3', 0), ('4_4', 0), ('4_5', 0), ('4_6', 0), ('4_7', 0), ('4_8', 0), ('4_9', 0), ('4_10', 0), ('4_11', 0), ('4_12', 0), ('5_1', 0), ('5_2', 0), ('5_3', 0), ('5_4', 0), ('5_5', 0), ('5_6', 0), ('5_7', 0), ('5_8', 0), ('5_9', 0), ('5_10', 0), ('5_11', 0), ('5_12', 0), ('6_1', 0), ('6_2', 0), ('6_3', 0), ('6_4', 0), ('6_5', 0), ('6_6', 0), ('6_7', 0), ('6_8', 0), ('6_9', 0), ('6_10', 0), ('6_11', 0), ('6_12', 0)]
 	def test_RunDummy(self):
 		self.assertEqual(0, 0)
 
@@ -15,8 +15,8 @@ class ClusterDictionaryTest(unittest.TestCase):
 	def test_createDefaultEntryToClusterDict_ShouldBe(self):
 		
 		value = ClusterDictionary.createDefaultEntryToClusterDict().items()
-		answer= [('0_0', 0), ('0_1', 0), ('0_2', 0), ('0_3', 0), ('0_4', 0), ('0_5', 0), ('0_6', 0), ('0_7', 0), ('0_8', 0), ('0_9', 0), ('0_10', 0), ('0_11', 0), ('1_0', 0), ('1_1', 0), ('1_2', 0), ('1_3', 0), ('1_4', 0), ('1_5', 0), ('1_6', 0), ('1_7', 0), ('1_8', 0), ('1_9', 0), ('1_10', 0), ('1_11', 0), ('2_0', 0), ('2_1', 0), ('2_2', 0), ('2_3', 0), ('2_4', 0), ('2_5', 0), ('2_6', 0), ('2_7', 0),('2_8', 0), ('2_9', 0), ('2_10', 0), ('2_11', 0), ('3_0', 0), ('3_1', 0), ('3_2', 0), ('3_3', 0), ('3_4', 0), ('3_5', 0), ('3_6', 0), ('3_7', 0), ('3_8', 0), ('3_9', 0), ('3_10', 0), ('3_11', 0), ('4_0', 0), ('4_1', 0), ('4_2', 0), ('4_3', 0), ('4_4', 0), ('4_5', 0), ('4_6', 0), ('4_7', 0), ('4_8', 0), ('4_9', 0), ('4_10', 0), ('4_11', 0), ('5_0', 0), ('5_1', 0), ('5_2', 0), ('5_3', 0), ('5_4', 0), ('5_5', 0), ('5_6', 0), ('5_7', 0), ('5_8', 0), ('5_9', 0), ('5_10', 0), ('5_11', 0)]
-		self.assertEqual(answer, value)
+		
+		self.assertEqual(self.default_entry, value)
 ########## Unit Test addDefaultEntryToClusterDict
 
 ########## Unit  addNewEntry(issuer_id)
@@ -35,47 +35,47 @@ class ClusterDictionaryTest(unittest.TestCase):
 ########## Unit Test contains(issuer_id)
 
 ########## Unit  Test getIssuerSpecificItem(issuer_id, item)
-	def test_getIssuerSpecificItem_ShouldBe0_withIssuerID1234AndItem0_0(self):
+	def test_getIssuerSpecificItem_ShouldBe0_withIssuerID1234AndItem1_1(self):
 		self.dict.addNewEntry('1234')
-		self.assertEqual(0,self.dict.getIssuerSpecificItem('1234','0_0'))		
+		self.assertEqual(0,self.dict.getIssuerSpecificItem('1234','1_1'))		
 ########## Unit  Test getIssuerSpecificItem(issuer_id, item)		
 
 ########## Unit  Test incrementIssuerItem(issuer_id, item)		
-	def test_incrementIssuerItem_ShouldBe1_with_IssuerID1234andItem0_0(self):
+	def test_incrementIssuerItem_ShouldBe1_with_IssuerID1234andItem1_1(self):
 		self.dict.addNewEntry('1234')
-		self.assertEqual(0,self.dict.getIssuerSpecificItem('1234','0_0'))				
-		self.dict.incrementIssuerItem('1234','0_0')
-		self.assertEqual(1,self.dict.getIssuerSpecificItem('1234','0_0'))		
+		self.assertEqual(0,self.dict.getIssuerSpecificItem('1234','1_1'))				
+		self.dict.incrementIssuerItem('1234','1_1')
+		self.assertEqual(1,self.dict.getIssuerSpecificItem('1234','1_1'))		
 
-	def test_incrementIssuerItem_ShouldBe5_with_IssuerID1111andItem0_0(self):
+	def test_incrementIssuerItem_ShouldBe5_with_IssuerID1111andItem1_1(self):
 		self.dict.addNewEntry('1111')
-		self.assertEqual(0,self.dict.getIssuerSpecificItem('1111','0_0'))				
+		self.assertEqual(0,self.dict.getIssuerSpecificItem('1111','1_1'))				
 		for counter in xrange(5):
-			self.dict.incrementIssuerItem('1111','0_0')
-		self.assertEqual(5,self.dict.getIssuerSpecificItem('1111','0_0'))				
+			self.dict.incrementIssuerItem('1111','1_1')
+		self.assertEqual(5,self.dict.getIssuerSpecificItem('1111','1_1'))				
 ########## Unit  Test incrementIssuerItem(issuer_id, item)		
 
 ########## Unit  Test addToIssuerItem(issuer_id, item)		
-	def test_addToIssuerItem_ShouldBe1_with_IssuerID1234andItem0_0(self):
+	def test_addToIssuerItem_ShouldBe1_with_IssuerID1234andItem1_1(self):
 		self.dict.addNewEntry('1234')
-		self.assertEqual(0,self.dict.getIssuerSpecificItem('1234','0_0'))				
-		self.dict.addToIssuerItem('1234','0_0',1)
-		self.assertEqual(1,self.dict.getIssuerSpecificItem('1234','0_0'))		
+		self.assertEqual(0,self.dict.getIssuerSpecificItem('1234','1_1'))				
+		self.dict.addToIssuerItem('1234','1_1',1)
+		self.assertEqual(1,self.dict.getIssuerSpecificItem('1234','1_1'))		
 
-	def test_addToIssuerItem_ShouldBe5_with_IssuerID1111andItem0_0(self):
+	def test_addToIssuerItem_ShouldBe5_with_IssuerID1111andItem1_1(self):
 		self.dict.addNewEntry('1111')
-		self.assertEqual(0,self.dict.getIssuerSpecificItem('1111','0_0'))					
-		self.dict.addToIssuerItem('1111','0_0', 5)
-		self.dict.addToIssuerItem('1111','0_0', 5)
-		self.assertEqual(10,self.dict.getIssuerSpecificItem('1111','0_0'))				
+		self.assertEqual(0,self.dict.getIssuerSpecificItem('1111','1_1'))					
+		self.dict.addToIssuerItem('1111','1_1', 5)
+		self.dict.addToIssuerItem('1111','1_1', 5)
+		self.assertEqual(10,self.dict.getIssuerSpecificItem('1111','1_1'))				
 ########## Unit  Test addToIssuerItem(issuer_id, item)		
 
 ########## Unit  Test exportCSV()		
 	def test_exportCSV(self):
 		newdict = ClusterDictionary()
 		newdict.addNewEntry('1111')
-		newdict.addToIssuerItem('1111','0_0', 8)		
-		newdict.addToIssuerItem('1111','0_0', 8)				
+		newdict.addToIssuerItem('1111','1_1', 8)		
+		newdict.addToIssuerItem('1111','1_1', 8)				
 		newdict.addNewEntry('2111')
 		newdict.addToIssuerItem('2111','1_2', 18)				
 		newdict.addNewEntry('3111')
