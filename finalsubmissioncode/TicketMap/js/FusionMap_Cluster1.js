ac65bc66981b0a0113dcfef279eeb0ee638896b2
christopherfan <chrisfan@outlook.com>
2014-05-14 20:36:25 -0700

    Final Submission Code Files

---
+++ finalsubmissioncode/TicketMap/js/FusionMap_Cluster1.js
@@ -0,0 +1,113 @@
+ 
+var map;
+var layer_0;
+var layer_1;
+
+
+
+/* Initialize
+	1) Google Maps
+	2) Default Query
+	3) Legend
+*/
+function initialize() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+	center: new google.maps.LatLng(40.72039940933483, -74.00116651619265),
+	zoom: 13,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  layer_0 = new google.maps.FusionTablesLayer({
+	query: {
+	  select: "col9",
+	  // from: "1rqIGtxbFqXFlq0IdeRyxmQlcAaPQd_ZdvkyYYyWb"
+	  from: "1CRk1NfA8UPxXjPyST8B6UkHs17o3gtPag-qHQaeO"
+	},
+	map: map,
+	styleId: 3,
+	templateId: 5,
+      options: {
+        styleId: 2,
+        templateId: 2
+      }	
+  });
+
+	// Create the legend and display on the map
+	var legend = document.createElement('div');
+	legend.id = 'legend';
+	legendContent(legend);
+	legend.index = 1;
+	map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);  
+  
+    $(function() {
+		$( "#search-string_dateFrom" ).datepicker();
+		$( "#search-string_dateTo" ).datepicker();
+	});
+}
+/** 
+	Change Map Forms Fusion API Query and makes request to Google Maps API
+**/
+function changeMap_0() {
+  var whereClause;
+  var searchString_issuer = document.getElementById('search-string_issuer').value.replace(/'/g, "\\'");
+  var searchString_dateFrom = document.getElementById('search-string_dateFrom').value.replace(/'/g, "\\'");
+  var searchString_dateTo = document.getElementById('search-string_dateTo').value.replace(/'/g, "\\'");
+  
+  var searchString_code = document.getElementById('search_violation_code').value.replace(/'/g, "\\'");
+  
+  if (searchString_dateFrom != '--Select--') {    
+	whereClause = "'issue_date' >= '" + searchString_dateFrom + "'";
+  }
+  if (searchString_dateTo != '') {
+	whereClause = whereClause +" AND 'issue_date' <= '" + searchString_dateTo + "'";
+  }
+
+  //Extract Violation Codes
+  if (searchString_code != '--Select--') {
+	var searchCode_array = $("#search_violation_code ").val()
+	var searchCode_string = "'" + searchCode_array[0] + "'";
+	if( searchCode_array.length >1){
+		$.each(searchCode_array.slice(1,searchCode_array.length), function(key, value){			
+			searchCode_string +=  ",'"+ value + "'";
+			console.log("Adding Code String", searchCode_string );
+		});
+	}
+	console.log("Code String", searchCode_string );
+	whereClause = whereClause   + " AND " + "'violation_code' IN (" +searchCode_string+ " )";
+	// whereClause = whereClause  + " AND 'violation_code'='" + searchString_code + "'";
+  }  
+
+  // Extract Issuer
+  if (searchString_issuer != '--Select--') {
+	var searchIssuer_array = $("#search-string_issuer").val()
+	var searchIssuer_string = "'" + searchIssuer_array[0] + "'";
+	if( searchIssuer_array.length >1){
+		$.each(searchIssuer_array.slice(1,searchIssuer_array.length), function(key, value){			
+			searchIssuer_string +=  ",'"+ value + "'";
+			console.log("Adding Code String", searchIssuer_string );
+		});
+	}
+	console.log("Code String", searchIssuer_string );
+	whereClause = whereClause   + " AND " + "'issuer_code' IN (" +searchIssuer_string+ " )";
+	// whereClause = whereClause  + " AND 'violation_code'='" + searchString_code + "'";
+  }  
+
+  // if (searchString_issuer != '--Select--') {
+	// whereClause = whereClause  +" AND 'issuer_code'='" + searchString_issuer + "'";
+  // }    
+  
+  
+  /* Make Google Fusion API Query */
+  console.log(">>>>>>>>>>>>>", whereClause);
+  layer_0.setOptions({
+	query: {
+	  select: "col9",
+	  from: "1CRk1NfA8UPxXjPyST8B6UkHs17o3gtPag-qHQaeO",
+	  where: whereClause
+	}
+  });
+  
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
---
+++ finalsubmissioncode/TicketMap/js/PlaceMarkers.js
@@ -0,0 +1,121 @@
+var myCenter=new google.maps.LatLng(40.79795821,-73.96370172);
+var markersArray = new Array();
+var point1 = '251 W 97th St	New York	New York';
+var point2 = '101 Manhattan Ave	New York	New York';
+var point3 = '622 W 132nd St	New York	New York';
+var point4 ='274 W 145th St	New York	New York';
+var points = [point1,point2, point3, point4];
+var geocoder;
+
+function initialize()
+	{
+	geocoder = new google.maps.Geocoder();
+	var mapProp = {
+	center:myCenter,
+	zoom:13,
+	mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	
+$(document).ready(function() { 
+	console.log("Ready");
+	$.getJSON( "data/json/json_geo3.json", function( data ) {
+		$.each(data, function(key, value){
+			console.log(key);
+			  $('#date_select').append('<option value='+key +'>'+key+ '</option>');
+		});
+	});
+	
+});	
+
+var pinColor = "FE7569";
+var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(10, 34)); 
+  
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+$('#date_select').on('change', function() {
+  // alert( this.value ); // or $(this).val()
+  printMarkers($("#date_select").val());
+});
+ 
+function printMarkers(date){ 
+	$.getJSON( "data/json/json_geo3.json", function( data ) {
+		  var geo_array = [];
+		  console.log(">>>>>>>>>>>>>>>>>>>>>>>", date);
+		  var date_entries = data[date]
+		  console.log(date_entries);
+		  console.log(date_entries);
+		  clearOverlays();
+		  $.each( date_entries, function( key, val ) {
+			// items.push( "<li id='" + key + "'>" + val + "</li>" );
+			console.log(val['lat'], val['long']);
+			var time_violation = val['time'] + "_" + val['violation']
+			var myCenter=new google.maps.LatLng(val['lat'], val['long']);
+			
+			// setTimeout(function (){
+				var marker=new google.maps.Marker({
+					position:myCenter,
+					icon:pinImage,
+					title:time_violation
+					});		
+				// marker.setMap(map);
+				 //something you want delayed
+			 // }, 100);
+			geo_array.push(myCenter);				
+			markersArray.push(marker);
+		  });
+		var dayPath = new google.maps.Polyline({
+		path: geo_array,
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+		});
+		dayPath.setMap(map);
+	});	
+}
+
+function clearOverlays() {
+  if (markersArray) {
+    for (i in markersArray) {
+      markersArray[i].setMap(null);
+    }
+  }
+  markersArray= new Array();
+}
+
+// $.getJSON( "js/jsonAddress.json", function( data ) {
+  // var items = [];
+  // console.log(">>>>>>>>>>>>>>>>>>>>>>>");
+  // $.each( data, function( key, val ) {
+    // // items.push( "<li id='" + key + "'>" + val + "</li>" );
+	// console.log(val['address']);
+	// // codeAddress(val['address'], val['time']);
+  // });
+// });
+
+
+function codeAddress(value, time) {
+  <!-- var address = document.getElementById('address').value; -->
+  address = value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+		  title:time
+      });    
+	} else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {    
+		setTimeout(function() {
+			codeAddress(address);
+		}, 200);
+	}else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+}
+google.maps.event.addDomListener(window, 'load', initialize);
